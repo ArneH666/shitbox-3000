05c8058397b63d528870930fe3aed91cae07354a
Arne Hilbig <111295854+ArneH666@users.noreply.github.com>
2024-04-30 13:14:46 +0200

    Refactored code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <Arduino.h>
-#include <list>
+#include "lighting/lighting.h"
 
-int LIGHT_SENSOR_PIN 4;
+
+typedef struct {
+    bool red;
+    bool green;
+    bool blue;
+} light_value;
+
+
+int LIGHT_SENSOR_PIN = 4;
 
 int FRONT_LEFT[] = {16, 17, 5};
 int FRONT_RIGHT[] = {0, 2, 15};
 int REAR_LEFT[] = {26, 33, 32};
 int REAR_RIGHT[] = {27, 14, 12};
+
+bool indicator_left = true;
+bool indicator_right = true;
+bool indicated = false;
 
 int input_pins[] = {LIGHT_SENSOR_PIN};
 int output_pins[] = {
@@ -16,13 +28,16 @@ int output_pins[] = {
     REAR_RIGHT[0], REAR_RIGHT[1], REAR_RIGHT[2]
 };
 
+void set_lighting();
+void set_lights(int[3], light_value);
+
 void setup() {
     Serial.begin(9600);
-    for (int i = 0; i < 1; i++) {
-        pinMode(input_pins[i], INPUT);
+    for (int input_pin : input_pins) {
+        pinMode(input_pin, INPUT);
     }
-    for (int i = 0; i < sizeof(output_pins) / sizeof(output_pins[0]); i++) {
-        pinMode(output_pins[i], OUTPUT);
+    for (int output_pin : output_pins) {
+        pinMode(output_pin, OUTPUT);
     }
 }
 
@@ -33,101 +48,37 @@ void loop() {
 
 void set_lighting() {
     int lightValue = analogRead(LIGHT_SENSOR_PIN);
-    // Serial.println(lightValue);
+    Serial.println(lightValue);
 
-    if (lightValue < 100) {
-        is_day = false;
-    } else {
-        is_day = true;
+    bool is_night = lightValue <= 100;
+
+    light_value front_left = {false, false, false};
+    light_value front_right = {false, false, false};
+    light_value rear_left = {false, false, false};
+    light_value rear_right = {false, false, false};
+
+
+    if (is_night) {
+        front_left = {true, true, true};
+        front_right = {true, true, true};
+        rear_left = {true, false, false};
+        rear_right = {true, false, false};
     }
 
-    bool red_fl = false;
-    bool red_fr = false;
-    bool red_rl = false;
-    bool red_rr = false;
-    bool green_fl = false;
-    bool green_fr = false;
-    bool green_rl = false;
-    bool green_rr = false;
-    bool blue_fl = false;
-    bool blue_fr = false;
-    bool blue_rl = false;
-    bool blue_rr = false;
-
-    if (is_day) {
-        red_fl = false;
-        green_fl = false;
-        blue_fl = false;
-        red_fr = false;
-        green_fr = false;
-        blue_fr = false;
-        red_rl = false;
-        green_rl = false;
-        blue_rl = false;
-        red_rr = false;
-        green_rr = false;
-        blue_rr = false;
-    } else {
-        red_fl = true;
-        green_fl = true;
-        blue_fl = true;
-        red_fr = true;
-        green_fr = true;
-        blue_fr = true;
-        red_rl = true;
-        green_rl = false;
-        blue_rl = false;
-        red_rr = true;
-        green_rr = false;
-        blue_rr = false;
-    }
-
-    // Left LEDs
     if (indicator_left && !indicated) {
-        red_fl = true;
-        green_fl = true;
-        blue_fl = false;
-        red_rl = true;
-        green_rl = true;
-        blue_rl = false;
-    } else if (indicator_left && indicated && is_day) {
-        red_fl = false;
-        green_fl = false;
-        blue_fl = false;
-        red_rl = false;
-        green_rl = false;
-        blue_rl = false;
-    } else if (indicator_left && indicated && !is_day) {
-        red_fl = true;
-        green_fl = true;
-        blue_fl = true;
-        red_rl = true;
-        green_rl = false;
-        blue_rl = false;
+        front_left = {true, true, false};
+        rear_left = {true, true, false};
+    } else if (indicator_left && indicated && is_night) {
+        front_left = {true, true, true};
+        rear_right = {true, false, false};
     }
 
-    // Right LED
     if (indicator_right && !indicated) {
-        red_fr = true;
-        green_fr = true;
-        blue_fr = false;
-        red_rr = true;
-        green_rr = true;
-        blue_rr = false;
-    } else if (indicator_right && indicated && is_day) {
-        red_fr = false;
-        green_fr = false;
-        blue_fr = false;
-        red_rr = false;
-        green_rr = false;
-        blue_rr = false;
-    } else if (indicator_right && indicated && !is_day) {
-        red_fr = true;
-        green_fr = true;
-        blue_fr = true;
-        red_rr = true;
-        green_rr = false;
-        blue_rr = false;
+        front_right = {true, true, false};
+        rear_right = {true, true, false};
+    } else if (indicator_right && indicated && is_night) {
+        front_right = {true, true, true};
+        rear_right = {true, false, false};
     }
 
     if (indicator_left || indicator_right) {
@@ -136,32 +87,14 @@ void set_lighting() {
         indicated = false;
     }
 
-    set_front_left_led(red_fl, green_fl, blue_fl);
-    set_front_right_led(red_fr, green_fr, blue_fr);
-    set_rear_left_led(red_rl, green_rl, blue_rl);
-    set_rear_right_led(red_rr, green_rr, blue_rr);
+    set_lights(FRONT_LEFT, front_left);
+    set_lights(FRONT_RIGHT, front_right);
+    set_lights(REAR_LEFT, rear_left);
+    set_lights(REAR_RIGHT, rear_right);
 }
 
-void set_front_left_led(bool r, bool g, bool b) {
-    digitalWrite(RED_FL, !r);
-    digitalWrite(GREEN_FL, !g);
-    digitalWrite(BLUE_FL, !b);
-}
-
-void set_front_right_led(bool r, bool g, bool b) {
-    digitalWrite(RED_FR, !r);
-    digitalWrite(GREEN_FR, !g);
-    digitalWrite(BLUE_FR, !b);
-}
-
-void set_rear_left_led(bool r, bool g, bool b) {
-    digitalWrite(RED_RL, !r);
-    digitalWrite(GREEN_RL, !g);
-    digitalWrite(BLUE_RL, !b);
-}
-
-void set_rear_right_led(bool r, bool g, bool b) {
-    digitalWrite(RED_RR, !r);
-    digitalWrite(GREEN_RR, !g);
-    digitalWrite(BLUE_RR, !b);
+void set_lights(int led_pins[3], light_value led_values) {
+    digitalWrite(led_pins[0], !led_values.red);
+    digitalWrite(led_pins[1], !led_values.green);
+    digitalWrite(led_pins[2], !led_values.blue);
 }
