a7a61cc91ece555279cf998a13abc0569d671b7d
Arne Hilbig <111295854+ArneH666@users.noreply.github.com>
2024-05-04 22:47:26 +0200

    Adding bluetooth

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,41 @@
 #include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
+#include <task.h>
 
 #include "pins/pins.h"
 #include "lighting/lighting.h"
+#include "bluetooth/bluetooth.h"
 
 
 Lighting lighting;
+Bluetooth bluetooth = {};
+
+
+void receive_bluetooth_messages();
 
 
 void setup() {
     Serial.begin(9600);
 
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 3; j++)
-            pinMode(lightPins[i][j], OUTPUT);
+    bluetooth.BTSerial.begin("Shitbox-3000");
+
+    for (auto & lightPin : lightPins)
+        for (int j : lightPin)
+            pinMode(j, OUTPUT);
 }
 
 void loop() {
+    String msg = bluetooth.get_message();
+    if (msg != "") {
+        Serial.println(msg);
+        for (char c : msg) {
+            int ch = c;
+            Serial.print(ch);
+            Serial.print(" ");
+        }
+        Serial.println();
+    }
+
     int light_val = analogRead(lightSensorPin);
 
     lighting.set_lighting(light_val);
