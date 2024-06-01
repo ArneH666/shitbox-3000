6bc38dfa62c6425e9e5b1afb94b50e42aef1c28a
Arne Hilbig <111295854+ArneH666@users.noreply.github.com>
2024-06-01 20:37:40 +0200

    Current dev status

--- src/main.cpp
+++ src/main.cpp
@@ -3,40 +3,59 @@
 #include "pins/pins.h"
 #include "lighting/lighting.h"
 #include "bluetooth/bluetooth.h"
-
+#include "distance/ultrasonic_distance.h"
 
 Lighting lighting;
+UltrasonicDistance distance;
 Bluetooth bluetooth = {};
 
-
-void receive_bluetooth_messages();
-
-
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(9600);
 
-    bluetooth.BTSerial.begin("Shitbox-3000");
+  bluetooth.BTSerial.begin("Shitbox-3000");
 
-    for (auto &lightPin: lightPins)
-        for (int j: lightPin)
-            pinMode(j, OUTPUT);
+  for (auto &lightPin : lightPins) {
+    for (int j : lightPin) {
+      pinMode(j, OUTPUT);
+    }
+  }
+
+  pinMode(ultrasonic_trigger_pin, OUTPUT);
+
+  for (int pin : inputPins) {
+    pinMode(pin, INPUT);
+  }
 }
 
 void loop() {
-    String msg = bluetooth.get_message();
-    if (msg != "") {
-        Serial.println(msg);
-    }
+  digitalWrite(ultrasonic_trigger_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ultrasonic_trigger_pin, LOW);
+  Serial.println(digitalRead(ultrasonic_sensor_pins[0]));
 
-    if (msg == "ind_left")
-        lighting.invertIndicatorLeft();
-    else if (msg == "ind_right")
-        lighting.invertIndicatorRight();
-    else if (msg == "reverse")
-        lighting.invertReverse();
+//  receive_bluetooth_messages();
+//
+//  int light_val = analogRead(lightSensorPin);
+//
+//  Distances dist = distance.get_distances();
+//
+////  Serial.println(dist.front);
+//
+//  lighting.setLighting(light_val);
+  delay(1);
+}
 
-    int light_val = analogRead(lightSensorPin);
+void receive_bluetooth_messages() {
+  String msg = bluetooth.get_message();
+  if (msg != "") {
+    Serial.println(msg);
+  }
 
-    lighting.setLighting(light_val);
-    delay(1);
+  if (msg == "ind_left") {
+    lighting.invertIndicatorLeft();
+  } else if (msg == "ind_right") {
+    lighting.invertIndicatorRight();
+  } else if (msg == "reverse") {
+    lighting.invertReverse();
+  }
 }
